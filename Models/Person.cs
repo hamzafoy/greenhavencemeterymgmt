187d27ac129d981b2afb569fab3802a0baa5abbb
hamzafoy <hamzaoflouisville@gmail.com>
2022-07-17 23:07:31 -0400

    saving progress so far.

--- Models/Person.cs
+++ Models/Person.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GreenhavenMgmt.Models;
 
 public class Person
@@ -5,6 +7,7 @@ public class Person
     public int id { get; set; }
     public string firstName { get; set; }
     public string lastName { get; set; }
+    [Required(ErrorMessage = "Must provide a year")]
     public int birthYear { get; set; }
     public int birthMonth { get; set; }
     public int birthDay { get; set; }
